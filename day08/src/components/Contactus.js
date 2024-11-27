3db504f77f041b88ec9551dfead0a03c5edbf908
PasinduNuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2024-11-27 11:57:04 +0530

    Create Contactus.js

---
+++ day08/src/components/Contactus.js
@@ -0,0 +1,19 @@
+import '../assets/basic.css'
+export default function Contactus(){
+    return (
+        <>
+        <header className="heder">
+        <h1>Contact Us</h1>
+        </header>
+        <p class='paragraph'>
+        Have questions or need assistance? We’re here to help! Whether you’re a student looking for guidance, a developer seeking resources, or simply curious about our classes, feel free to reach out.
+
+        Here’s how you can connect with us:
+        <br></br>
+        📧 Email: support@javascriptpracticals.com<br></br>
+        📞 Phone: +0000000<br></br>
+        📍 Address: JavaScript Learning Hub, 123 Coding Street, Tech City, Sri Lanka
+        </p>
+        </>
+    );
+}
