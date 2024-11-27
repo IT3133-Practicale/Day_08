fd3acf0b7bb9ec063ce934970c4c05e617de6867
PasinduNuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2024-11-27 11:57:10 +0530

    Create Home.js

---
+++ day08/src/components/Home.js
@@ -0,0 +1,20 @@
+import '../assets/basic.css'
+export default function Home(){
+    return (
+        <>
+        <header className="heder">
+        <h1>Welcome to Your JavaScript Practical Class!</h1>
+        </header>
+        <p class='paragraph'>
+        Explore the fascinating world of JavaScript through hands-on practice! This platform is designed to help you:
+            <br></br>
+            Learn by Doing: Write, test, and debug real code to understand JavaScript concepts better.
+            Engage with Projects: Build interactive applications and solve practical challenges.<br></br>
+            Master Fundamentals: Deep dive into topics like variables, functions, loops, and DOM manipulation.
+            Experiment Freely: A safe environment to experiment with code and push your limits.
+            Get Started Today:<br></br>
+            Dive into tutorials, practical exercises, and project ideas. Whether you're a beginner or advancing your skills, this is the perfect place to grow as a developer.
+        </p>
+        </>
+    );
+}
