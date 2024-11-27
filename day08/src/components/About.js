de114cafc60ee9b0e710275f62c19df734720619
PasinduNuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2024-11-27 11:57:01 +0530

    Create About.js

---
+++ day08/src/components/About.js
@@ -0,0 +1,19 @@
+import '../assets/basic.css'
+export default function About(){
+    return (
+        <>
+        <header className="heder">
+        <h1>About Us</h1>
+        </header>
+        <p class='paragraph'>
+        Welcome to the JavaScript Practical Class, a hub for hands-on learning and exploration in the world of web development! Our mission is to provide an engaging platform where learners can sharpen their programming skills, tackle real-world challenges, and build confidence in coding.
+        <br></br>
+        We believe that the best way to learn JavaScript is by practicing it. That’s why we focus on:
+        <br></br>
+        Interactive Learning: Offering step-by-step exercises, projects, and challenges.
+        Skill Development: Equipping students with essential programming techniques and problem-solving skills.
+        Real-World Applications: Bridging the gap between theory and practice through practical examples and tasks.
+        </p>
+        </>
+    );
+}
