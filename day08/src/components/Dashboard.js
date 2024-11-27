07d4f4a0a08765a3156825f62fb16d00fdc6c234
PasinduNuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2024-11-27 11:57:07 +0530

    Create Dashboard.js

---
+++ day08/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import '../assets/basic.css'
+export default function Dashboard(){
+    return (
+        <>
+        <header className="heder">
+        <h1>Dashboard</h1>
+        </header>
+        <p className="paragraph">Welcome to dashboard</p>
+        </>
+    );
+}
